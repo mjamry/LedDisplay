b5c68314dbcff37880f4aa3cb69ec04d67443ae5
Michal <michal.jamry@gmail.com>
2015-01-29 21:00:52 +0100

    Added timer1 channel A CTC mode interrupt handler. Also some additional code to test interupt.

--- Main.c
+++ Main.c
@@ -1,6 +1,8 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+
 #define DELAY 100
 #define CHAR_NUMBER 2
 #define CHAR_LENGTH 8
@@ -8,6 +10,19 @@
 #define CLK 0x40
 #define OUT 0x20
 #define RST 0x10
+
+uint8_t INDEX = 0;
+
+ISR(TIMER1_COMPA_vect)
+{
+	PORTD |= 0x04;
+	if(INDEX == 0)
+		INDEX = 1;
+	else
+		INDEX = 0;
+	//_delay_ms(100);
+}
+
 
 int main(void)
 {
@@ -18,9 +33,21 @@ int main(void)
     DDRB = 0xFF;
     DDRD = 0xFF;
 
+    //set CTC mode
+    TCCR1B |= (1<<WGM12);
+    //set prescaler to 1024
+    TCCR1B |= ((1<<CS12) | (1<<CS10));
+    //set interupt flag
+    TIMSK |= (1<<OCIE1A);
+    //set counter value
+    OCR1A = 0x00FF;
+    //set global interrupt flag
+    sei();
+
+
     while(1)
     {
-
+    	PORTD &= ~(0x04);
         for(i=0;i<CHAR_NUMBER;i++)
         {
         	PORTD |= RST;
@@ -28,7 +55,7 @@ int main(void)
         	for(j=0;j<CHAR_LENGTH;j++)
         	{
         		PORTD |= CLK;
-				PORTB = charTab[i][j];
+				PORTB = charTab[INDEX][j];
 				_delay_us(DELAY);
 				PORTD &= ~(CLK);
 				_delay_us(DELAY);
